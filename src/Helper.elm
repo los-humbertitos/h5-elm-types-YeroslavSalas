9a0514f0848c3c8ec8661940978dcb476dbaf71e
Yeroslav Salas <up220432@alumnos.upa.edu.mx>
2026-03-01 23:45:38 -0600

    La tarea

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,58 @@
-module Helper exposing (..)
+module Helper exposing
+    ( AirplaneStatus(..)
+    , GradeStatus(..)
+    , airplaneScheduleAction
+    , airportAction
+    , categoricalGrade
+    )
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade grades =
+    List.map gradeToStatus grades
+
+
+gradeToStatus : Float -> GradeStatus
+gradeToStatus grade =
+    if grade < 0 then
+        Pending
+
+    else if grade >= 7 then
+        Approved
+
+    else
+        Failed
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        OnTime ->
+            "Esperar"
+
+        Delayed ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+        Cancelled ->
+            "Pedir reembolso"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction statuses =
+    List.map airplaneScheduleAction statuses
